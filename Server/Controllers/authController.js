772f3353b4a4aac60d3d8596dcd406718cc73f9f
Yogesh Sharma <yogesh.sharma12041@gmail.com>
2025-03-25 13:29:25 +0530

    fix bug

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -82,8 +82,8 @@ export const login = async(req, res) => {
       
           res.cookie('token', token, {
               httpOnly: true,
-              secure: process.env.NODE_ENV === 'production',
-              sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+              secure: true,
+              sameSite: "None",
               maxAge: 7 * 24 * 60 * 60 * 1000
           });
 
