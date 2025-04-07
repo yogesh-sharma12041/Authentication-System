545f59daf43647cb4553bc3695188c23116eb5a0
Yogesh Sharma <yogesh.sharma12041@gmail.com>
2025-04-07 22:45:08 +0530

    Added Loader

--- Client/src/Components/Loading.jsx
+++ Client/src/Components/Loading.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div className='text-cyan-200'>Loading...</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
+      <div className="relative">
+        <div className="w-20 h-20 rounded-full border-4 border-t-indigo-600 border-b-indigo-600 border-l-transparent border-r-transparent animate-spin"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-indigo-700 font-semibold">
+          Loading...
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
